334ebed0dc201636fc8c1ce535cd580c8477fb22
tere15 <kristiina.horneman@gmail.com>
2016-01-08 15:56:20 +0200

    Version 1.0.2

---
+++ FrontEnd/mainModule.js
@@ -0,0 +1,55 @@
+
+
+var main_module = angular.module('main_module',['ngRoute', 'ngResource']);
+
+
+main_module.config(function($routeProvider){
+    
+    $routeProvider.when('/',{
+        // Triggeröityy kun url latautuu eli tullaan '/' kohtaan
+        templateUrl:'partial_mainView.html', //toteuttaa tietyn osan dokumenttia (siksi partial)
+        //controller:'controllerMain'
+        //factory:'loginFactory'
+    
+    }).when('/list',{
+        
+        templateUrl:'partial_productView.html', //toteuttaa tietyn osan dokumenttia (siksi partial)
+        controller:'controllerLogin'
+    }
+
+                        
+                       /*).when('/list',{
+        // Jos näkymä tarvii tietoa backendiltä, tehään kontrolleri ja liimataan tässä
+        //yhteen näkymä ja kontrolleri. Tehään myös factory, jos esim.
+        // 2. näkymässä tarvitaan samaa dataa. Factoryssa kaikki data tallessa.
+    
+        templateUrl:'partial_dataView.html',
+        controller: 'friendDataController',
+        resolve:{loginRequired: loginRequired}
+        
+    
+    ).when('/new',{
+        
+        templateUrl:'partial_newView.html',
+        controller: 'friendNewController',
+        resolve:{loginRequired: loginRequired}
+
+
+        
+    }).when('/modify',{
+        
+        templateUrl:'partial_modifyView.html',
+        controller: 'friendModifyController',
+        resolve:{loginRequired: loginRequired}
+        
+    
+    }).when('/delete',{
+        
+        templateUrl:'partial_deleteView.html',
+        controller: 'friendDeleteController',
+        resolve:{loginRequired: loginRequired}
+        
+    }); */
+    
+    
+});
